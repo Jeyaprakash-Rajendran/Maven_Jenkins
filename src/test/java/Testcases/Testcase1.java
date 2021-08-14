afba2de290c27cfd6d9eeb35572ae4fc9b694c43
Jeyaprakash-Rajendran <rjprakash202@gmail.com>
2021-08-14 21:15:28 +0530

    first commit

---
+++ src/test/java/Testcases/Testcase1.java
@@ -0,0 +1,19 @@
+package Testcases;
+
+import org.testng.annotations.Test;
+
+public class Testcase1 {
+	
+	@Test
+	public void method1()
+	{
+		System.out.println("M1 ");
+	}
+
+
+	@Test
+	public void method2()
+	{
+		System.out.println("M2 ");
+	}
+}
---
+++ src/test/java/Testcases/Testcase2.java
@@ -0,0 +1,19 @@
+package Testcases;
+
+import org.testng.annotations.Test;
+
+public class Testcase2 {
+
+	@Test
+	public void method3()
+	{
+		System.out.println("M3 ");
+	}
+
+
+	@Test
+	public void method4()
+	{
+		System.out.println("M4 ");
+	}
+}
